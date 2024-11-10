f0a927d8dd939c7df64b1e078e70d4e417aa5eb9
harshmistry3172 <harshmistry3172@gmail.com>
2024-11-10 19:25:54 +0530

    changes cors code

--- backend/index.js
+++ backend/index.js
@@ -8,15 +8,23 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-const allowedOrigins = ['*'];
+// const allowedOrigins = ['*'];
 
-app.use(
-  cors({
-    origin: allowedOrigins, // Set allowed origins
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Set allowed methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Set allowed headers
-  })
-);
+// app.use(cors());
+
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://todolistapp-rosy.vercel.app/');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // Pass to next layer of middleware
+  next();
+})
 
 app.use(express.json());
 
